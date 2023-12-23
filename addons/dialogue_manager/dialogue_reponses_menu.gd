a5c6a6ba122b5a5fe7a121e10cefb8af6da08d75
Amethyst-szs <Amethyst-UwU@protonmail.com>
2023-12-22 16:36:24 -0800

    Use interact key for response menu

--- addons/dialogue_manager/dialogue_reponses_menu.gd
+++ addons/dialogue_manager/dialogue_reponses_menu.gd
@@ -118,7 +118,5 @@ func _on_response_gui_input(event: InputEvent, item: Control, item_index: int) -
 
 	get_viewport().set_input_as_handled()
 
-	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
-		response_selected.emit(_responses[item_index])
-	elif event.is_action_pressed("ui_accept") and item in get_menu_items():
+	if event.is_action_pressed("interact") and item in get_menu_items():
 		response_selected.emit(_responses[item_index])
